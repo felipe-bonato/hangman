b6c9ed14bc89e2e4ec2afc3728b0ae1e4ced9b3e
Felipe Bonato <felipe13.bonato@gmail.com>
2021-08-20 17:40:49 -0300

    Reworked db for async

--- db.js
+++ db.js
@@ -4,103 +4,56 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 const dbName = 'hangman';
 
-/**
- * @param cb function(db_connection, errors)
- */
-function getBbConnection(cb)
-{
+exports.getAllWords = async function getAllWords() {
 	const client = new MongoClient(url);
-	client.connect((err) => {
-		if(err){
-			cb(null, new Error('Could not connect to db.\n' + err))
-		} else {
-			cb(client.db(dbName), null);
-		}
-		setTimeout(() => client.close(), 500); // <- Isso é uma estupidez
-	});
+	try {
+		const conn = await client.connect();
+		const words = await conn.db(dbName).collection('words').find({}).toArray();
+		return words.map((word_pair) => word_pair.word);
+	} catch {
+		throw new Error('Could not get words from database');
+	} finally {
+		client.close();
+	}
 }
 
-/**
- * 
- * @param cb function(words, err)
- */
-exports.getAllWords = function getAllWords(cb) {
-	getBbConnection((db, err) => {
-		if(err){
-			cb(null, err);
-		} else {
-			db.collection('words').find({}).toArray((err, docs) => {
-				if(err){
-					cb(null, new Error('Could not get words from database'));
-				} else {
-					cb(docs.map((word_pair) => word_pair.word), null);
-				}
-			});
-		}
-	});
+exports.getRandomWord = async function getRandomWord() {
+	const client = new MongoClient(url);
+	try {
+		const conn = await client.connect();
+		const words = await conn.db(dbName).collection('words').find({}).toArray();
+		const words_arr = words.map((word_pair) => word_pair.word);
+		return words_arr[Math.floor(Math.random() * words_arr.length)];
+	} catch {
+		throw new Error('Could not get words from database');
+	} finally {
+		client.close();
+	}
 }
 
-/**
- * @param cb function(word, err)
- */
-exports.getRandomWord = function getRandomWord(cb) {
-	getBbConnection((db, err) => {
-		if(err){
-			cb(null, err);
-		} else {
-			db.collection('words').find({}).toArray((err, docs) => {
-				if(err){
-					cb(null, new Error('Could not get words from database'));
-				} else {
-					words = docs.map((word_pair) => word_pair.word)
-					cb(words[Math.floor(Math.random() * words.length)], null);
-				}
-			});
-		}
-	});
+exports.insertWord = async function insertWord(word) {
+	const client = new MongoClient(url);
+	const word = word.toUpperCase();
+	try {
+		const conn = await client.connect();
+		const stored_words = await getAllWords();
+		if(stored_words.includes(word)) throw new Error('Word already exists');
+		conn.db(dbName).collection('words').insertOne({'word': word});
+	} catch {
+		throw new Error('Could not get words from database');
+	} finally {
+		client.close();
+	}
 }
 
-exports.insertWord = function insertWord(word, cb) {
-	word = word.toUpperCase();
-	getBbConnection((db, err) => {
-		if(err){
-			cb(null, err);
-		} else {
-			getAllWords((words, err) => {
-				if(err){
-					cb(null, new Error('Could not get words from database'));
-				} else if(words.includes(word)){
-					cb(null, new Error('Word already into database into database'));
-				} else {
-					db.collection('words').insertOne({
-						'word': word,
-					}, (err, r) => {
-						if(err){
-							cb(null, new Error('Could not insert word into database'));
-						} else if(r.insertedCount > 1){
-							cb(null, new Error('Could not insert single word into database'));
-						} else {
-							cb(r, null);
-						}		
-					});
-				}
-			});
-		}
-	});
-}
-
-function deleteAllWords(cb) {
-	getBbConnection((db, err) => {
-		if(err){
-			cb(null, err);
-		} else {
-			db.collection('words').deleteMany({}, (err, result) => {
-				if(err){
-					cb(null, new Error('Could not connect to DB.\n' + err));
-				} else {
-					cb(result, null);
-				}
-			});
-		}
-	});
+async function deleteAllWords() {
+	const client = new MongoClient(url);
+	try {
+		const conn = await client.connect();
+		conn.db(dbName).collection('words').deleteMany({});
+	} catch {
+		throw new Error('Could not get words from database');
+	} finally {
+		client.close();
+	}
 }
